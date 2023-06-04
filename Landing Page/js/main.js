00152d71e6d84c0a6a92025a1c3be65bb0305171
LizLogan <93852431+LizLogan@users.noreply.github.com>
2023-06-04 18:52:04 +0700

    deleted

--- Landing Page/js/main.js
+++
@@ -1,9 +0,0 @@
-const keAtas = document.querySelector('.ke-atas');
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 200) {
-    keAtas.classList.add('active');
-  } else {
-    keAtas.classList.remove('active');
-  }
-});
